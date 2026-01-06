9bce462e6b37d98d71a36d138f55ffa12370e325
Hidejoshi <mykytarud.ir@gmail.com>
2026-01-06 20:23:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@ import os
 import sqlite3
 from datetime import datetime, timedelta
 import pytz
-from flask import Flask
-import threading
 
 
 from telegram import Update
@@ -464,18 +462,6 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("\n".join(msg))
         return
 
-
-def keep_alive():
-    app_flask = Flask(__name__)
-
-    @app_flask.route("/")
-    def home():
-        return "OK"
-
-    port = int(os.environ.get("PORT", 10000))
-    app_flask.run(host="0.0.0.0", port=port)
-
-threading.Thread(target=keep_alive, daemon=True).start()
 
 # ================== RUN ==================
 
